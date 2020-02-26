4e1bbc7d5ed98b69c02dfda9827a0103dc67dde0
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-02-26 16:08:52 +0900

    Changed the fitting model; need to make some variables for models

--- X-ray Analysis/Suzaku/scripts/make_spec.sh
+++ X-ray Analysis/Suzaku/scripts/make_spec.sh
@@ -2,10 +2,10 @@
 
 source /Users/changhyun/suzaku/research/proc/scripts/env_SUZAKU.sh
 
-for BURST in 0 1 2 3 4 5 6 7 8; do #0 1 2 3 4 5 6 7 8
+for BURST in 0; do #0 1 2 3 4 5 6 7 8
 
 	cd $SPEC_DIR/$BURST
-	rm $SPEC_DIR/$BURST/burst.xcm
+	rm $SPEC_DIR/$BURST/burst_compTT.xcm
 
 	$XSPEC <<EOF
 data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha
@@ -15,25 +15,37 @@ backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha
 setplot energy
 ignore 1-3: **-1.0 12.0-**
 statistic chi
-model wabs*bbodyrad
+model wabs*compTT
+
+0 -1
+1.13404
+50 -1
+
+2 -1
+1.0
 
 
 
 
 
-0.02
+
+2.0
 
 
-0.03
+
+
+
+
+3.0
 que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
 
 cpd /xs
 iplot ldata ratio
-rescale y 0.01 5
+rescale y 0.0001 0.1
 label top Specturm of the burst $BURST
-hardcopy burst_${BURST}_spec.ps/cps
+hardcopy burst_${BURST}_spec_compTT.ps/cps
 quit
-save all burst
+save all burst_compTT
 EOF
 
   cp $SPEC_DIR/$BURST/*.ps $RESULT_DIR/$BURST
