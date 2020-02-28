94f30893938cac941c68549214ce57e4f3872c57
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-02-28 14:34:10 +0400

    Make an fitting result as a separate file according to the model

--- X-rayAnalysis/Suzaku/scripts/make_spec.sh
+++ X-rayAnalysis/Suzaku/scripts/make_spec.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source /Users/changhyun/GitHub/astrophysics/X-rayAnalysis/Suzaku/scripts/env_SUZAKU.sh
+source /Users/changhyun/GitHub/astrophysics/X-rayAnalysis/Suzaku/scripts/spec_models.sh
 
-for BURST in 0; do #0 1 2 3 4 5 6 7 8
+for BURST in 1 2 3 4 5 6 7 8; do #0 1 2 3 4 5 6 7 8
 
 	cd $SPEC_DIR/$BURST
-	rm $SPEC_DIR/$BURST/burst_compTT.xcm
+	rm $SPEC_DIR/$BURST/burst_$MODEL.xcm
 
 	$XSPEC <<EOF
 data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha
@@ -15,20 +16,21 @@ backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha
 setplot energy
 ignore 1-3: **-1.0 12.0-**
 statistic chi
-$COMPTT
+$MODEL_VAR
 que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
-
 cpd /xs
 iplot ldata ratio
-rescale y 0.0001 0.1
-label top Specturm of the burst $BURST
-hardcopy burst_${BURST}_spec_compTT.ps/cps
+rescale y 0.05 7
+label top Specturm of the burst $BURST using $MODEL fitting model
+hardcopy burst_${BURST}_spec_${MODEL}.ps/cps
 quit
-save all burst_compTT
+save all burst_${MODEL}
 EOF
 
-  cp $SPEC_DIR/$BURST/*.ps $RESULT_DIR/$BURST
+	$PS2PDF burst_${BURST}_spec_${MODEL}.ps
+  cp $SPEC_DIR/$BURST/burst_${BURST}_spec_${MODEL}.pdf $RESULT_DIR/$BURST
 
 done
 
 exit 0
+#
