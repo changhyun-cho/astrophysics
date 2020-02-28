2268386b642c124ad81baeb5514a4023358a72fd
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-02-28 14:34:30 +0400

    rebin the PHA file

--- X-rayAnalysis/Suzaku/scripts/rebin_pha.sh
+++ X-rayAnalysis/Suzaku/scripts/rebin_pha.sh
@@ -3,13 +3,13 @@
 source /Users/changhyun/GitHub/astrophysics/X-rayAnalysis/Suzaku/scripts/env_SUZAKU.sh
 
 REBIN="group 0 255 2
-group 256 511 32
+group 256 511 64
 group 512 1023 32
-group 1024 2047 32
-group 2048 2559 32
-group 2560 3071 64
-group 3072 3583 128
-group 3584 4095 128
+group 1024 2047 64
+group 2048 2559 128
+group 2560 3071 256
+group 3072 3583 256
+group 3584 4095 256
 "
 
 for BURST in 0 1 2 3 4 5 6 7 8 ; do #0 1 2 3 4 5 6 7 8
