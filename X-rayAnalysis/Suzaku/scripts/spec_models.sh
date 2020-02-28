67fa9d4bcfe1fda675b5badfd0776a2ed13eae80
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-02-28 14:34:21 +0400

    Add more models

--- X-rayAnalysis/Suzaku/scripts/spec_models.sh
+++ X-rayAnalysis/Suzaku/scripts/spec_models.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-COMPTT="model wabs*compTT
+source /Users/changhyun/GitHub/astrophysics/X-rayAnalysis/Suzaku/scripts/env_SUZAKU.sh
 
-0 -1
-1.13404
-50 -1
+if [ $MODEL='compTT' ]; then
 
-2 -1
-1.0
+  MODEL_VAR='model wabs*compTT
 
+  0 -1
+  1.13404
+  50 -1
 
-
-
-
-
-2.0
+  2 -1
+  1.0
 
 
 
 
 
 
-3.0
-"
-COMPBB=""
+  2.0
+
+
+
+
+
+
+  3.0
+  '
+elif [ $MODEL='compbb' ]; then
+  COMPBB='model wabs*compbb
+
+
+  50 -1
+
+  0.01
+
+
+
+
+  0.02
+
+
+
+
+  0.03
+  '
+
+fi
