4d2eaf12eb0e8916b44daa3e963f9ad8bf02c1f3
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-03-07 00:33:16 +0900

    Include the exception

--- X-rayAnalysis/Suzaku/scripts/make_spec.sh
+++ X-rayAnalysis/Suzaku/scripts/make_spec.sh
@@ -3,14 +3,14 @@
 source /Users/changhyun/GitHub/astrophysics/X-rayAnalysis/Suzaku/scripts/env_SUZAKU.sh
 source /Users/changhyun/GitHub/astrophysics/X-rayAnalysis/Suzaku/scripts/spec_models.sh
 
-echo $MODEL_VAR
-
-for BURST in 1; do #0 1 2 3 4 5 6 7 8
+for BURST in 0 1 2 3 4 5 6 7 8; do #0 1 2 3 4 5 6 7 8
 
 	cd $SPEC_DIR/$BURST
 	rm $SPEC_DIR/$BURST/burst_$MODEL.xcm
 
-	$XSPEC <<EOF
+	if [ "$BURST" == "0" ]; then
+
+		$XSPEC <<EOF
 data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha
 resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf
 arf 1 xis0.arf 2 xis1.arf 3 xis3.arf
@@ -21,7 +21,28 @@ statistic chi
 $MODEL_VAR
 que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
 cpd /xs
-iplot ldata ratio
+iplot ldata delchi
+rescale y 0.0008 0.2
+label top Specturm of the burst $BURST using $MODEL fitting model
+hardcopy burst_${BURST}_spec_${MODEL}.ps/cps
+quit
+save all burst_${MODEL}
+EOF
+
+	else
+
+		$XSPEC <<EOF
+data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha
+resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf
+arf 1 xis0.arf 2 xis1.arf 3 xis3.arf
+backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha
+setplot energy
+ignore 1-3: **-1.0 12.0-**
+statistic chi
+$MODEL_VAR
+que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
+cpd /xs
+iplot ldata delchi
 rescale y 0.05 7
 label top Specturm of the burst $BURST using $MODEL fitting model
 hardcopy burst_${BURST}_spec_${MODEL}.ps/cps
@@ -29,10 +50,11 @@ quit
 save all burst_${MODEL}
 EOF
 
+	fi
+
 	$PS2PDF burst_${BURST}_spec_${MODEL}.ps
   cp $SPEC_DIR/$BURST/burst_${BURST}_spec_${MODEL}.pdf $RESULT_DIR/$BURST
 
 done
 
 exit 0
-#
