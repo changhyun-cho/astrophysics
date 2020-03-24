4df99958a65cc9b9a6b2115ca519985ee3d02b40
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-03-24 21:57:29 +0900

    included the PIN spectrum

--- X-rayAnalysis/Suzaku/scripts/make_spec.sh
+++ X-rayAnalysis/Suzaku/scripts/make_spec.sh
@@ -11,18 +11,18 @@ for BURST in 0 1 2 3 4 5 6 7 8; do #0 1 2 3 4 5 6 7 8
 	if [ "$BURST" == "0" ]; then
 
 		$XSPEC <<EOF
-data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha
-resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf
-arf 1 xis0.arf 2 xis1.arf 3 xis3.arf
-backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha
+data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha 4:4 hxd_pin.pha
+resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf 4 ae_hxd_pinhxnome9_20100731.rsp
+arf 1 xis0.arf 2 xis1.arf 3 xis3.arf 4 hxd_pin.arf_pin.arf
+backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha 4:4 back_hxd_pin.pha
 setplot energy
-ignore 1-3: **-1.0 12.0-**
+ignore 1-3: **-1.0
 statistic chi
 $MODEL_VAR
 que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
 cpd /xs
 iplot ldata delchi
-rescale y 0.0005 0.5
+rescale y 0.001 4
 label top Specturm of the burst $BURST using $MODEL fitting model
 hardcopy burst_${BURST}_spec_${MODEL}.ps/cps
 quit
@@ -32,12 +32,13 @@ EOF
 	else
 
 		$XSPEC <<EOF
-data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha
-resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf
-arf 1 xis0.arf 2 xis1.arf 3 xis3.arf
-backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha
+data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha 4:4 hxd_pin.pha
+resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf 4 ae_hxd_pinhxnome9_20100731.rsp
+arf 1 xis0.arf 2 xis1.arf 3 xis3.arf 4 hxd_pin.arf_pin.arf
+backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha 4:4 back_hxd_pin.pha
 setplot energy
 ignore 1-3: **-1.0 12.0-**
+ignore 4: **-18.0 40.0-**
 statistic chi
 $MODEL_VAR
 que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
@@ -53,11 +54,11 @@ EOF
 	fi
 
 	$PS2PDF burst_${BURST}_spec_${MODEL}.ps
-  cp $SPEC_DIR/$BURST/burst_${BURST}_spec_${MODEL}.pdf $RESULT_DIR/$BURST
+  mv $SPEC_DIR/$BURST/burst_${BURST}_spec_${MODEL}.pdf $RESULT_DIR/$BURST
 
 done
 
 exit 0
-
-# ignore 1-3: **-1.0 1.7-2.4 12.0-**
+# ignore 1-3: **-1.0 12.0-**
+# ignore 1-3: **-1.0 1.7-1.9 2.2-2.4 12.0-**
 # if [ "$BURST" == "0" ] || [ "$BURST" == "0" ]; then
