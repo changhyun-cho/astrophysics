edb9b6fa440ba1de0c3619adfbf4ffaecce1aece
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-03-31 23:35:51 +0900

    adjust for the hxd spectrum

--- X-rayAnalysis/Suzaku/scripts/make_spec.sh
+++ X-rayAnalysis/Suzaku/scripts/make_spec.sh
@@ -14,9 +14,10 @@ for BURST in 0 1 2 3 4 5 6 7 8; do #0 1 2 3 4 5 6 7 8
 data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha 4:4 hxd_pin.pha
 resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf 4 ae_hxd_pinhxnome9_20100731.rsp
 arf 1 xis0.arf 2 xis1.arf 3 xis3.arf 4 hxd_pin.arf_pin.arf
-backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha 4:4 back_hxd_pin.pha
+backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha 4:4 back_hxd_pin_expcor.pha
 setplot energy
-ignore 1-3: **-1.0
+ignore 1-3: **-1.0 12.0-**
+ignore 4: **-18.0 40.0-**
 statistic chi
 $MODEL_VAR
 que no; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit ; renorm ; fit
@@ -35,7 +36,7 @@ EOF
 data 1:1 xis0_rebin.pha 2:2 xis1_rebin.pha 3:3 xis3_rebin.pha 4:4 hxd_pin.pha
 resp 1 xis0.rmf 2 xis1.rmf 3 xis3.rmf 4 ae_hxd_pinhxnome9_20100731.rsp
 arf 1 xis0.arf 2 xis1.arf 3 xis3.arf 4 hxd_pin.arf_pin.arf
-backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha 4:4 back_hxd_pin.pha
+backgrnd 1:1 back_xis0_rebin.pha 2:2 back_xis1_rebin.pha 3:3 back_xis3_rebin.pha 4:4 back_hxd_pin_expcor.pha
 setplot energy
 ignore 1-3: **-1.0 12.0-**
 ignore 4: **-18.0 40.0-**
