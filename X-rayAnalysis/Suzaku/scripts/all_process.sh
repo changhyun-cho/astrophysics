be80b403aa2d33b95479b00662cf14fbe16dd31d
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-04-01 01:02:02 +0900

    included the HXD PIN analysis

--- X-rayAnalysis/Suzaku/scripts/all_process.sh
+++ X-rayAnalysis/Suzaku/scripts/all_process.sh
@@ -8,3 +8,10 @@ bash make_pha_back.sh
 bash make_rmf_arf.sh
 bash rebin_pha.sh
 bash make_spec.sh
+
+bash make_pha_pin.sh
+bash make_pha_back_pin.sh
+bash make_arf_pin.sh
+bash rebin_pha_pin.sh
+#need to adjust the exposure time of background
+bash make_spec.sh
