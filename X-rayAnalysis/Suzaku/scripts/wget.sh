1c12a111a22505a418f379c10e6615df3834ef26
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-08-12 00:04:30 +0900

    Data download script from Archive

---
+++ X-rayAnalysis/Suzaku/scripts/wget.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -f
+opt='-nv -m -np -nH --cut-dirs=3 -P .'
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/503011010/ #1E...
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/501056010/ #CXOGCS J174445.5-295042
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/100027040/ #KS1741-293
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/100037030/ #KS1741-293
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/409011030/ #Sgr A East
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/409011050/ #Sgr A East
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/403009010/ #Arches cluster
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/100027050/ #1E1743.1-2843
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/100037050/ #1E1743.1-2843
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/501040020/ #G0.61+0.01
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/501040010/ #G0.61+0.01
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/502051010/ #HD 161507 = G0.9+0.1
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/502020010/ #SAX J1748.2−2808 = SGR D SNR
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/501060010/ #AX J1749.1-2733
+#wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/406076010/ #4U 1705-44
+wget $opt -R "index.html*" https://darts.isas.jaxa.jp/pub/suzaku/ver3.0/403044020/ #GX 301-2
+#No CXOGC J174645.3-281546
