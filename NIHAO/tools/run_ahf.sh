a66967d8cc9c70e7c4a07dbb9c5b50a0e8691dba
Changhyun Cho <cc6881@nyu.edu>
2024-07-03 11:38:59 +0400

    added interval

--- NIHAO/tools/run_ahf.sh
+++ NIHAO/tools/run_ahf.sh
@@ -10,9 +10,10 @@ AHF=/home/cc6881/bin/AHF
 TARGET=$1
 DIR=$(dirname "$TARGET")/
 FILE=$(basename "$TARGET")
+INTERVAL=16
 MASS=$(echo "$FILE" | cut -d'.' -f1).$(echo "$FILE" | cut -d'.' -f2)
 SNAP=$(echo "$FILE" | cut -d'.' -f3)
-N_FILE=$(echo "$SNAP / 16" | bc)  # Perform the division and save the result to a variable
+N_FILE=$(echo "$SNAP / $INTERVAL" | bc)  # Perform the division and save the result to a variable
 
 echo "Directory: $DIR"
 echo "Mass: $MASS"
@@ -21,7 +22,7 @@ cd $DIR
 
 for ((i=0; i<N_FILE; i++)); do
     INDEX=$(($i + 1))           # Increment i by 1 for correct file numbering
-    INDEX_FORMAT=$(printf "%05d" $(($INDEX * 16)))  # Format number to 5 digits with leading zeros
+    INDEX_FORMAT=$(printf "%05d" $(($INDEX * $INTERVAL)))  # Format number to 5 digits with leading zeros
     F_NAME="${DIR}${MASS}.${INDEX_FORMAT}"    # Construct FILE
     echo "$F_NAME"
     sed "s#SNAPSHOT#$F_NAME#" AHFinput.in > AHF.input
